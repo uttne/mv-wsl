ce24e671c1a2d3539ec398fa44fcdff8248cf06c
uttne <uttne.mkr@gmail.com>
2022-04-21 22:41:29 +0900

    init

---
+++ main.ps1
@@ -0,0 +1,19 @@
+﻿# https://programwiz.org/2021/06/26/how-to-move-wsl2-d-drive-from-c/#toc5
+
+Param(
+    $Name
+)
+
+if(-not $Name){
+    $Name = Read-Host
+    if(-not $Name){
+        throw "'Name' is empty."
+    }
+}
+
+Write-Host $Name
+
+$DestPath = Join-Path $PSScriptRoot $Name
+$TarPath = Join-Path $PSScriptRoot "ubuntu-20.04.tar"
+
+wsl --import "$Name" "$DestPath" "$TarPath"  --version 2
